500f9d18f30d89555feda9f101647803506f538d
MemduhAlpsoy <240101009@ogrenci.yalova.edu.tr>
2025-05-17 14:14:28 +0300

    neblm

---
+++ Memduh/scripts/common/game.gd
@@ -0,0 +1,5 @@
+extends Node2D
+
+
+func _on_hurt_box_2_dead() -> void:
+	%ColorRect.visible = true
--- Memduh/scripts/common/player.gd
+++ Memduh/scripts/common/player.gd
@@ -1,8 +1,10 @@
+#player.gd
+
 extends CharacterBody2D
 
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const JUMP_VELOCITY = -450.0
 var facingRight = true
 
 
@@ -12,7 +14,7 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
